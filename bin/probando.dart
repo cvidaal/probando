33d6f096d6e422aeb467b4d3eb5679450b0c4980
Carlos Vidal <carlosvsedet@gmail.com>
2024-04-17 15:22:49 +0200

    hola

---
+++ bin/probando.dart
@@ -0,0 +1,5 @@
+import 'package:probando/probando.dart' as probando;
+
+void main(List<String> arguments) {
+  print('Hello world: ${probando.calculate()}!');
+}
---
+++ test/probando_test.dart
@@ -0,0 +1,8 @@
+import 'package:probando/probando.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
